1507f7d2f7fd1396d1f2f60e6adce9c2ed44bfe2
Kyle <kemartin1@hawkmail.hfcc.edu>
2016-06-02 11:38:29 -0400

    Initial Commit

---
+++ Car.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClass
+{
+    class Car
+    {
+        private String year;
+        private String make;
+        private String model;
+        private int _speed;
+        private double _tank;
+        
+        public Car(String y, String make, String model)
+        {
+            year = y;
+            this.make = make;
+            this.model = model;
+        }
+        public String getYear()
+        {
+            return year;
+        }
+        public String getMake()
+        {
+            return make;
+        }
+        public String getModel()
+        {
+            return model;
+        }
+        public void setYear(String y)
+        {
+            year = y;
+        }
+        public void setMake(String ma)
+        {
+            make = ma;
+        }
+        public void setModel(String mo)
+        {
+            model = mo;
+        }
+        public int Speed
+        {
+            get { return _speed; }
+            set { _speed = value; }
+        }
+        public double Tank
+        {
+            get { return _tank; }
+            set { _tank = value; }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarClass
+{
+    public partial class Form1 : Form
+    {
+        Car myCar;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtMake.Text == String.Empty || txtYear.Text == String.Empty || txtModel.Text == String.Empty)
+                    MessageBox.Show("All data must be filled!");
+                else
+                {
+                    createCar();
+                    label8.Text = txtYear.Text + " " + txtMake.Text + " " + txtModel.Text;
+                }
+            }
+                catch(Exception ex)
+            {
+                    MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnAccelerate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (myCar.Tank <= .25)
+                {
+                    MessageBox.Show("Your car needs to be filled with gas first!");
+                    myCar.Speed = 0;
+                    lblMph.Text = myCar.Speed.ToString();
+                }
+                else
+                {
+                    myCar.Speed += 5;
+                    myCar.Tank -= .25;
+                    lblMph.Text = myCar.Speed.ToString();
+                    lblGas.Text = myCar.Tank.ToString();
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void createCar()
+        {
+            myCar = new Car(txtYear.Text, txtMake.Text, txtModel.Text);
+        }
+
+        private void btnBrake_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (myCar.Tank <= .25)
+                {
+                    MessageBox.Show("Your car needs to be filled with gas first!");
+                    myCar.Speed = 0;
+                    lblMph.Text = myCar.Speed.ToString();
+                }
+                else if(myCar.Speed <= 5)
+                {
+                    MessageBox.Show("Someone is brake happy >:(");
+                    myCar.Speed = 0;
+                    lblMph.Text = myCar.Speed.ToString();
+                }
+                else
+                {
+                    myCar.Speed -= 5;
+                    myCar.Tank -= .10;
+                    lblMph.Text = myCar.Speed.ToString();
+                    lblGas.Text = myCar.Tank.ToString();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnFill_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (myCar.Tank >= 13)
+                    MessageBox.Show("Your car is Full!");
+                else
+                {
+                    myCar.Speed = 0;
+                    myCar.Tank += 1;
+                    lblGas.Text = myCar.Tank.ToString();
+                    lblMph.Text = myCar.Speed.ToString();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
